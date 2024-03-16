f758a051ee4d1e006688ba51d1c8d6e4a843164b
AndraxDev <dostapenko82@gmail.com>
2024-03-16 13:03:44 +0100

    Release

--- start-app.sh
+++ start-app.sh
@@ -9,4 +9,4 @@ docker build -t json-database-api:latest .
 
 # Run docker container
 echo "Running docker container json-database-api..."
-docker run -d -it --rm -p 5000:5000 json-database-api --network papi
+docker run -d -it --network papi --rm -p 5000:5000 json-database-api
